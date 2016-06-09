d7d0b0cf25772de35563bc8d86807073765d74cf
davidmccoy <david.mccoy@gmail.com>
2016-06-08 20:04:23 -0700

    added infinite scroll

--- functions.php
+++ functions.php
@@ -329,4 +329,24 @@ function custom_excerpt_length( $length ) {
 } 
 add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
+/**
+ * Add infinite scroll.
+ */
+
+function mytheme_infinite_scroll_init() {
+  add_theme_support( 'infinite-scroll', array(
+    'type' => 'scroll',
+    'container' => 'homepage',
+    'render' => 'mytheme_infinite_scroll_render',
+    'posts_per_page' => 12,
+    'footer' => false,
+  ) );
+} 
+add_action( 'init', 'mytheme_infinite_scroll_init' );
+
+function mytheme_infinite_scroll_render() {
+  while ( have_posts() ) : the_post();
+    get_template_part( 'homepage_content' );
+  endwhile;
+}
 ?>
---
+++ homepage_content.php
@@ -0,0 +1,50 @@
+     
+<div class="homepagecontent"> 
+  <?php
+    $post_image_id = get_post_thumbnail_id($post_to_use->ID);
+    if ($post_image_id) {
+      $thumbnail = wp_get_attachment_image_src( $post_image_id, 'post-thumbnail', false);
+      if ($thumbnail) (string)$thumbnail = $thumbnail[0];
+    } 
+  ?>
+  <a href="<?php the_permalink() ?>">
+    <div class="featured_image" style="background: url('<?php echo $thumbnail; ?>') no-repeat center center; background-size: cover; height: 220px;">
+    </div>
+  </a>
+  <div class="homeinfo">
+    <div class="post-category">
+      <h4>
+        <?php the_category() ?>
+      </h4>
+    </div>
+    <div class="post-title">
+     <h3>
+        <a href="<?php the_permalink() ?>" rel="bookmark">
+          <?php the_title(); ?>
+        </a>
+      </h3>
+    </div>
+    <div class="home_excerpt">
+      <?php the_excerpt(); ?>
+    </div>
+  </div>
+  <?php if(of_get_option('display_socialhome') == '1') { ?>
+    <div class="social_links">
+        <div class="tweet_btn">
+            <a href="http://twitter.com/share" class="twitter-share-button"
+            data-url="<?php the_permalink(); ?>"
+            data-via="<?php echo of_get_option('social_twitter_url'); ?>"
+            data-text="<?php the_title(); ?>"
+            data-related=""
+            data-count="horizontal"><?php _e("Tweet", 'organicthemes'); ?></a>
+        </div>
+        <div class="like_btn">
+            <div class="fb-like" href="<?php the_permalink(); ?>" data-send="false" data-layout="button_count" data-width="100" data-show-faces="false"></div>
+        </div>
+        <div class="plus_btn">
+          <g:plusone size="medium"></g:plusone>
+        </div>
+    </div>
+  <?php } else { ?>
+  <?php } ?>  
+</div>
