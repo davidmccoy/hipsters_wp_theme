941dc8e4ec6b1e2820e5dcff08ab09500ff5b3b3
davidmccoy <david.mccoy@gmail.com>
2016-07-01 20:23:25 -0500

    updated author info and added author image

--- index.php
+++ index.php
@@ -4,8 +4,15 @@
 
 	<div id="content" class="left">
 
-		<div class="postarea">
-      <div class="title_page">
+		<div class="post-area">
+
+
+      <?php $meta_box = get_post_custom($post->ID); $video = $meta_box['custom_meta_video'][0]; ?>
+      <?php if ( $video ) : ?>
+        <div class="postvid_single"><?php echo $video; ?></div>
+      <?php else: ?>
+        <div class="postimg_single"><?php echo clean_wp_width_height(get_the_post_thumbnail(get_the_ID(),'post-feature')); ?></div>
+      <?php endif; ?>
 
 			<h4 class="category">
 				<?php the_category(', ') ?>
@@ -13,50 +20,61 @@
 			<h1 class="post-title">
 				<?php the_title(); ?>
 			</h1>
-
-			<?php $meta_box = get_post_custom($post->ID); $video = $meta_box['custom_meta_video'][0]; ?>
-			<?php if ( $video ) : ?>
-				<div class="postvid_single"><?php echo $video; ?></div>
-			<?php else: ?>
-				<div class="postimg_single"><?php echo clean_wp_width_height(get_the_post_thumbnail(get_the_ID(),'post-feature')); ?></div>
-			<?php endif; ?>
-
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-        <div class="postauthor">
-        	<?php
-        		$now = current_time('timestamp');
-        		$posted_at = get_the_time('U');
-        		$post_age = $now - $posted_at
-        	?>
-        	<?php if($post_age < 604800) { ?>
-	      		<p>
-	      			<?php _e("By", 'organicthemes'); ?> <?php the_author_posts_link(); ?> <?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . ' ago ';  ?> <?php edit_post_link(__("(Edit)", 'organicthemes'), '', ''); ?>
-	      		</p>
-        	<?php } else { ?>
-        		<p>
-        			<?php _e("By", 'organicthemes'); ?> <?php the_author_posts_link(); ?> <?php _e("on", 'organicthemes'); ?> <?php the_time(__("F j, Y", 'organicthemes'));  ?> <?php edit_post_link(__("(Edit)", 'organicthemes'), '', ''); ?>
-        		</p>
-        	<?php } ?>
+      <div class="post-author-container">
+        <div class="post-author-image" style="background-image: url('<?php echo get_wp_user_avatar_src($author, 66); ?> ');">
         </div>
-
+        <div class="post-author-info">
+          <div class="post-author">
+            <p>
+              <?php the_author_posts_link(); ?> 
+            </p>
+            <p>
+              <a href="<?php echo the_author_meta('user_url') ?>" rel="twitter" target="_blank">@<?php the_author_nickname(); ?></a>
+            </p>
+          </div>
+        </div>
       </div>
-      <?php 
-        $time_to_read =  ceil(wcount() / 275);
-        if ($time_to_read == 1) {
-      ?>
+      <div class="post-date">
+        <?php
+          $now = current_time('timestamp');
+          $posted_at = get_the_time('U');
+          $post_age = $now - $posted_at              
+        ?>
+
+        <?php if($post_age < 604800) { ?>
+          <p>
+            <?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . ' ago ';  ?>
+          </p>
+        <?php } else { ?>
+          <p>
+            <?php the_time(__("F j, Y", 'organicthemes'));  ?> 
+      		</p>
+        <?php } ?>
+        <div class="middot-spacer">
+          &middot; 
+        </div>
+        <?php 
+          $time_to_read =  ceil(wcount() / 275);
+          if ($time_to_read == 1) {
+        ?>
+          <p>
+            <?php echo $time_to_read . " minute to read" ?>
+          </p>
+        <?php    
+          } else {
+        ?>
+          <p>
+            <?php echo $time_to_read . " minutes to read" ?>
+          </p>
+        <?php 
+          }
+        ?>
         <p>
-          <?php echo $time_to_read . " minute to read" ?>
+          <?php edit_post_link(__("(Edit)", 'organicthemes'), '', ''); ?>
         </p>
-      <?php    
-        } else {
-      ?>
-        <p>
-          <?php echo $time_to_read . " minutes to read" ?>
-        </p>
-      <?php 
-        }
-      ?>
+      </div>
 
 			<?php the_content(__("Read More", 'organicthemes'));?>
 
